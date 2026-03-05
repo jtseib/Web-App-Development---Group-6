ed655da9c09236c4c0137374b60829e99d7eef8f
akakpodededometojudith-source <akakpodededometojudith@gmail.com>
2026-03-05 14:25:56 -0600

    Added FitTrack homepage and styling

--- JanGym/views.py
+++ JanGym/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+def trainers(request):
+    return render(request, 'trainers.html')
+
+def workouts(request):
+    return render(request, 'workouts.html')
