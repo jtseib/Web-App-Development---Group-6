8569b455ca60233f93a52654cfe1b78e161ce94d
TrevorOReilly1 <toreilly@unomaha.edu>
2026-04-18 12:00:24 -0500

    Unit Tests
    First round of unit tests, checking navigation and pages exist.

---
+++ DjangoGroupProjectGym/tests/unitTest_cancelSession.py
@@ -0,0 +1,50 @@
+
+
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+
+class ll_ATS(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def test_ll(self):
+        driver = self.driver
+        driver.maximize_window()
+        user = "testuser"
+        pwd = "asdfghjklQWERTYUIOP"
+        driver.get("http://127.0.0.1:8000/admin")
+        time.sleep(3)
+        elem = driver.find_element(By.ID,"id_username")
+        elem.send_keys(user)
+        elem = driver.find_element(By.ID,"id_password")
+        elem.send_keys(pwd)
+        time.sleep(3)
+        elem.send_keys(Keys.RETURN)
+        driver.get("http://127.0.0.1:8000")
+        time.sleep(3)
+
+        driver.find_element(By.XPATH, "//a[contains(., 'Dashboard')]").click()
+        driver.find_element(By.XPATH, "//a[contains(., 'View My Sessions')]").click()
+        driver.find_element(By.XPATH, "//a[contains(., 'Cancel')]").click()
+
+        time.sleep(5)
+        try:
+            elements = driver.find_elements(By.XPATH, "//button[normalize-space()='Back to Sessions']")
+
+        except NoSuchElementException:
+            driver.close()
+            self.fail("Dashboard page not found")
+
+        time.sleep(2)
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
